b492a6acbde0b8bbe4f5218ebd35add554514bfc
Huseni Sameer Jaker <103500196+sameerhuseni@users.noreply.github.com>
2023-03-15 23:22:03 +0530

    Update BackgroundColorChanger.js

--- src/Pages/BackgroundColorChanger.js
+++ src/Pages/BackgroundColorChanger.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import {useParams} from "react-router-dom";
+// import './Style.css';
 
 
 export const BackgroundColorChanger = () =>{
+    const params=useParams();
+    const mStyle={
+        backgroundColor: params.colorname,
+    }
+  
     return(
-       
+       <main id='main-style'style={mStyle}> Background Color Changer
+       </main>
     )
 }
