9e215890eae68423b32d68254031a7a222445f4c
Huseni Sameer Jaker <103500196+sameerhuseni@users.noreply.github.com>
2023-03-15 23:22:51 +0530

    Update Calculator.js

--- src/Pages/Calculator.js
+++ src/Pages/Calculator.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 
 
-export const Calculator = () =>{
+export const Calculator = () => {
+    const [params] = useSearchParams();
+    console.log(params.get('op'))
+    const fisrtNum = params.get('num1');
+    const secondNum = params.get('num2')
+    let ans = 0;
+    if (params.get('op') === '+')
+        ans = parseInt(fisrtNum) + parseInt(secondNum);
+    else if (params.get('op') === '/')
+        ans = fisrtNum / secondNum
+    else if (params.get('op') === '%')
+        ans = fisrtNum % secondNum
+    else if (params.get('op') === '*')
+        ans = fisrtNum * secondNum
+    else if (params.get('op') === '-')
+        ans = fisrtNum - secondNum
 
-    return(
-        
+    return (
+        <div>Your calculation Result is <span id="calc-result">{ans}</span></div>
     )
 }
